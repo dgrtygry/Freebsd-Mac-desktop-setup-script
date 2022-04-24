3891971106185392e0903f69c93f17661f051206
drigy982 <76979689+dgrtygry@users.noreply.github.com>
2022-04-24 14:50:27 -0500

    Update compile-pt3.sh

--- compile-pt3.sh
+++ compile-pt3.sh
@@ -1,6 +1,10 @@
-cd Downloads
+cd /home/admini/Downloads
 curl https://github.com/mszoek/airyx/archive/refs/heads/main.zip
 unzip /home/admini/Downloads/main.zip
-cd airyx-main
+cd /home/admini/Downloads/airyx-main
 Makefile && make.conf
 service netif restart
+cd /home/admini/Downloads
+curl https://raw.githubusercontent.com/dgrtygry/FreeBSD-Broadcom-4313-driver-port/main/broadcom4313-BSD.bz2
+unzip -d /home/admini/Downloads/broadcom4313-BSD.bz2
+pkg install /home/admini/Downloads/broadcom4313-BSD.pkg
