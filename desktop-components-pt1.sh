372381d84be9d99c1aaf545bb15bfbb89fdb63f8
drigy982 <76979689+dgrtygry@users.noreply.github.com>
2022-04-29 17:44:53 -0500

    Update desktop-components-pt1.sh

--- desktop-components-pt1.sh
+++ desktop-components-pt1.sh
@@ -1,4 +1,2 @@
-pkg update && pkg upgrade
-
-pkg install firefox latte-dock octopkg wifimgr networkmgr curl git virtualbox fusefs-hfsfuse hfsexplorer hfsutils libfshfs gpart neofetch
+pkg update && pkg upgrade && pkg install firefox octopkg wifimgr networkmgr curl git virt-manager fusefs-hfsfuse hfsexplorer hfsutils libfshfs gpart neofetch gnome3-lite sddm pulseaudio alsa-utils xfce4-appfinder sierra-gtk-themes vlc openshot py38-libopenshot libopenshot-audio epiphany && portsnap fetch && portsnap update && portsnap fetch update && pkg install nano neofetch fish 
 
